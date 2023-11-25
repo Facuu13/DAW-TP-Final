d34d3be12bd71fdae5eeb9d0ca64237f0e743bcb
Facundo Andrioli Villa <facundo.andrioli@keylab.com.ar>
2023-11-25 13:22:07 -0300

    se agrego el boton de agregar dispositivos como modal

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,6 +1,14 @@
+var M;
+class Main implements EventListenerObject{
 
-function SayHello(){
-    let current_value = document.getElementById("textarea_1") as HTMLInputElement;
-    let new_value = "Hello world!!!" + "\n" + current_value.value;
-    document.getElementById("textarea_1").innerHTML = new_value;
+    handleEvent(object: Event): void {
+    }
 }
+window.addEventListener("load",  ()=> {
+
+    var elemsModal = document.querySelectorAll('.modal');
+    M.Modal.init(elemsModal, null);
+
+    let main: Main = new Main();
+
+});
